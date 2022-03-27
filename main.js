1dfdb74fa0bc43b33412a6aba27eeb78237256e3
vkaash-sharma <vikasayushsharma@gmail.com>
2022-03-26 22:24:53 -0800

    srapping added to excel file

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595"
-
+const path = require("path");
+const fs = require("fs");
 const request = require("request");
 const cheerio = require("cheerio");
 const allMatchObj = require("./allMatch");
@@ -13,6 +14,12 @@ function cb (err , res , body){
     }else{
         handleHTML(body);
     }
+}
+
+
+let iplPath = path.join(__dirname,"IPL");
+if(!fs.existsSync(iplPath)){
+    fs.mkdirSync(iplPath);
 }
 
 
